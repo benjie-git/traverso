afbc334164cb56beca863eaa7ce67d61360b3b79
Ben Levitt <benjie@gmail.com>
2026-08-26 16:34:41 -0700

    Fix gain movement going haywire (integrating with mouse movement distance during the drag)

--- src/commands/Gain.cpp
+++ src/commands/Gain.cpp
@@ -137,7 +137,7 @@ void Gain::set_new_gain_numerical_input(float newGain)
 int Gain::process_mouse_move(qreal diffY)
 {
     qreal of = 0;
-    audio_sample_t dbFactor = coefficient_to_dB(m_newGain);
+    audio_sample_t dbFactor = coefficient_to_dB(m_origGain);
 
 
     if (dbFactor > -1) {
