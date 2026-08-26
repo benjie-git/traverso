7844811b08f6d659be9b890176a5b361f9a28326
Ben Levitt <benjie@gmail.com>
2026-08-26 09:08:05 -0700

    update build system

--- deploy-osx.sh
+++ deploy-osx.sh
@@ -27,14 +27,18 @@ echo "Using traverso binary: $BIN_PATH"
 MACDEPLOYQT=""
 if command -v macdeployqt >/dev/null 2>&1; then
     MACDEPLOYQT="$(command -v macdeployqt)"
-elif [ -x "/opt/homebrew/opt/qt@5/bin/macdeployqt" ]; then
-    MACDEPLOYQT="/opt/homebrew/opt/qt@5/bin/macdeployqt"
+elif [ -x "/opt/homebrew/opt/qt/bin/macdeployqt" ]; then
+    MACDEPLOYQT="/opt/homebrew/opt/qt/bin/macdeployqt"
+elif [ -x "/opt/homebrew/opt/qt@6/bin/macdeployqt" ]; then
+    MACDEPLOYQT="/opt/homebrew/opt/qt@6/bin/macdeployqt"
 elif [ -x "/opt/homebrew/bin/macdeployqt" ]; then
     MACDEPLOYQT="/opt/homebrew/bin/macdeployqt"
-elif [ -x "/usr/local/opt/qt@5/bin/macdeployqt" ]; then
-    MACDEPLOYQT="/usr/local/opt/qt@5/bin/macdeployqt"
-elif [ -x "/opt/local/libexec/qt5/bin/macdeployqt" ]; then
-    MACDEPLOYQT="/opt/local/libexec/qt5/bin/macdeployqt"
+elif [ -x "/usr/local/opt/qt/bin/macdeployqt" ]; then
+    MACDEPLOYQT="/usr/local/opt/qt/bin/macdeployqt"
+elif [ -x "/usr/local/opt/qt@6/bin/macdeployqt" ]; then
+    MACDEPLOYQT="/usr/local/opt/qt@6/bin/macdeployqt"
+elif [ -x "/opt/local/libexec/qt6/bin/macdeployqt" ]; then
+    MACDEPLOYQT="/opt/local/libexec/qt6/bin/macdeployqt"
 else
     echo "Error: macdeployqt not found."
     exit 1
